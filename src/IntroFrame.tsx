a8d428dfc82f806a38c803f15d735f326ccc419b
e548609 <chagai.friedlander@sbb.ch>
2022-12-10 22:53:54 +0100

    creating first components

---
+++ src/IntroFrame.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './IntroFrame.css';
+
+function IntroFrame() {
+  return (
+    <div className="IntroFrame">
+      <p style={{ fontSize: '1.9vw', marginBottom: '1%', paddingTop: '3%'}}>Hello I am Chagai</p>
+      <p style={{ fontSize: '1.1vw', marginTop: '3%'}}>You can call me by my middle name Joe</p>
+    </div>
+  );
+}
+
+export default IntroFrame;
