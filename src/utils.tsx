e94f3eecccc7e48f2453b64edcf17a285fa0d91c
e548609 <chagai.friedlander@sbb.ch>
2023-01-11 17:52:39 +0100

    add ring function

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,4 +1,3 @@
-import * as https from 'https';
 
 export async function sendMessage(message :string) {
   const data = JSON.stringify({
@@ -21,6 +20,36 @@ export async function sendMessage(message :string) {
   const matrixServer = "https://mx.chagai.website"
   const roomId = "!yxtimoKaUBDoOnQjts:chagai.website"
   const path = `/_matrix/client/r0/rooms/${roomId}/send/m.room.message`
+  const url = `${matrixServer}${path}`
+
+  const response = await fetch(url, options);
+  const dataResponse = await response.json();
+
+}
+
+export async function ring() {
+  const data = JSON.stringify({
+    call_id: '12345',
+    lifetime: 100,
+    offer: {sdp: "", type: "offer"},
+    version: 0,
+  });
+
+  const accessToken = "syt_Ym90_OFgkYeyofWPyXCwQjCIj_3BUAoC"
+  
+  const options = {
+    port: 8448,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${accessToken}`
+    },
+    body: data
+  };
+  
+  const matrixServer = "https://mx.chagai.website"
+  const roomId = "!yxtimoKaUBDoOnQjts:chagai.website"
+  const path = `/_matrix/client/r0/rooms/${roomId}/send/m.call.invite`
   const url = `${matrixServer}${path}`
 
   const response = await fetch(url, options);
